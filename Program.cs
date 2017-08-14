3a9dd24a4da2ea31f47c94e37f8858d6c7ac37f8
Robby Bourne <robbybourne1@gmail.com>
2017-08-14 12:17:55 -0400

    Too High Int Printing

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace dotnet_guessing_game
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var random = new Random();
+            int randomNumber = random.Next(0, 100);
+            Console.WriteLine(randomNumber);
+            var input = Convert.ToInt32(Console.ReadLine());
+            if (input > randomNumber);
+            {
+                Console.WriteLine("Number is too high!");
+            }
         }
     }
 }
